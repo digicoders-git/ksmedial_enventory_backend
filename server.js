3d01bddb1aeefd52db3388eb4ecf33f683212834
SHIVA VISHWAKARMA <raw66010@gmail.com>
2026-01-29 21:28:13 +0530

    url setup done

--- server.js
+++ server.js
@@ -13,10 +13,18 @@ connectDB();
 const app = express();
 
 // Middleware
+const allowedOrigins = process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(',') : ['http://localhost:5173'];
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
